1163d323df17285458371b7868f75293627684e4
jatur <turayjj@mail.uc.edu>
2024-09-25 15:10:00 -0400

    Init Commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        double cardBalance = 5000;
+
+        double interestRate = .17;
+
+        double interestMonthOne = cardBalance * interestRate;
+
+        double interestMonthTwo = (cardBalance * interestMonthOne) * interestRate;
+
+        System.out.println("The balance of your card is: " +cardBalance+
+
+                          System.lineSeparator()+
+
+                "Your interestrate is: " +interestRate+
+
+                        System.lineSeparator()+
+
+                "Your intrest due after one month: "+interestMonthOne+
+
+                        System.lineSeparator()+
+
+                "Your interest due after two months: "+interestMonthTwo
+
+                 );
     }
 }
